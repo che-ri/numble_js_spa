96ecfa5a4e12c339c8e1b368b69438f07c8fd3e6
che-ri <rnjs1135@gmail.com>
2023-01-20 04:43:04 +0900

    [feat] 404페이지 라이프사이클 함수 적용 (#9)

--- src/js/pages/NotFoundPage.js
+++ src/js/pages/NotFoundPage.js
@@ -1,9 +1,36 @@
 export default class NotFoundPage {
   constructor(dom) {
     this.dom = dom;
+    this.onInit();
+    this.onReady();
+  }
+
+  onInit() {
+    // 최초 진입
+    console.log("init");
+    this.dom.innerHTML = this.template();
+  }
+
+  onReady() {
+    //DOM 업데이트 이후
+    console.log("ready");
+  }
+
+  onWillUpdate() {
+    console.log("willUpdate");
+  }
+
+  onDidUpdate() {
+    console.log("didUpdate");
+  }
+
+  template() {
+    return `<div><div id="header"></div><h1>페이지를 찾을 수 없습니다.</h1>`;
   }
 
   render() {
-    this.dom.innerHTML = `<h1>페이지를 찾을 수 없습니다.</h1>`;
+    this.onWillUpdate();
+    this.dom.innerHTML = this.template();
+    this.onDidUpdate();
   }
 }
