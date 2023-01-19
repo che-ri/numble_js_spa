844878eb1f887f6c059c01d494353cbbac79a82b
che-ri <rnjs1135@gmail.com>
2023-01-20 01:59:31 +0900

    [feat] 메인페이지 api 연결, 결과를 뷰로 처리 (#6)

--- src/js/pages/MainPage.js
+++ src/js/pages/MainPage.js
@@ -2,24 +2,39 @@ import Header from "../components/Header.js";
 import icon from "../../constants/icon.js";
 import Router from "../router.js";
 import $ from "../selector.js";
+import api from "../../api/index.js";
 
 export default class MainPage {
   constructor(dom) {
     this.dom = dom;
+    this.posts = [];
+    this.onInit();
   }
 
-  init() {
+  onInit = async () => {
+    // dom 업데이트 전, init
+    try {
+      const response = await api.getPosts();
+
+      if (response.data.code !== 200) throw Error();
+      this.posts = response.data.data.posts;
+      this.render();
+    } catch (e) {}
+  };
+
+  onReady() {
+    //dom이 업데이트된 이후
     $("#go-write-btn").addEventListener("click", () => {
       Router.instance.push(Router.instance.pages.write.url);
     });
   }
 
-  card() {
+  card(post) {
     return `<div class="card">
-    <img class="card-img" loading="lazy"/>
+    <img class="card-img" loading="lazy" src="${post.image}"/>
     <div class="card-text-box">
-      <strong class="body1">신년 계획</strong>
-      <span class="body2 ellipsis">2022년 계획 세우셨나요?</span>
+      <strong class="body1">${post.title}</strong>
+      <span class="body2 ellipsis">${post.content}</span>
     </div>
     </div>`;
   }
@@ -30,13 +45,16 @@ export default class MainPage {
         <button id="go-write-btn" class="btn main-btn">${
           icon.write
         }<span class="main-btn-text body1">새 글 작성하기</span></button>
-      <div class="cards-container"><div class="cards">${this.card()}</div></div>
+      <div class="cards-container"><div class="cards">${this.posts.reduce(
+        (acc, cur) => (acc += this.card(cur)),
+        ""
+      )}</div></div>
     </div>`;
   }
 
   render() {
     this.dom.innerHTML = this.template();
     new Header($("#header")).render();
-    this.init();
+    this.onReady();
   }
 }
