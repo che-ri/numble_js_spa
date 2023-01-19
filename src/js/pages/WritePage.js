e5662696584a912e92d8fbe154f53d1d67a2bd0e
che-ri <rnjs1135@gmail.com>
2023-01-20 03:04:16 +0900

    [feat] 작성페이지 라이프사이클 적용 (#9)

--- src/js/pages/WritePage.js
+++ src/js/pages/WritePage.js
@@ -1,12 +1,40 @@
-import Router from "../router.js";
+import Header from "../components/Header";
+import $ from "../selector";
 
 export default class WritePage {
   constructor(dom) {
     this.dom = dom;
+    this.onInit();
+  }
+
+  onInit() {
+    // 최초 진입
+    console.log("init");
+    this.dom.innerHTML = this.template();
+    new Header($("#header"));
+  }
+
+  onReady() {
+    //DOM 업데이트 이후
+    console.log("ready");
+  }
+
+  onWillUpdate() {
+    console.log("willUpdate");
+  }
+
+  onDidUpdate() {
+    console.log("didUpdate");
+  }
+
+  template() {
+    return `<div><div id="header"></div><h1>작성페이지</h1>`;
   }
 
   render() {
-    this.dom.innerHTML = `<div><div id="header"></div><h1>작성페이지</h1>`;
-    Router.instance.render();
+    this.onWillUpdate();
+    this.dom.innerHTML = this.template();
+    new Header($("#header"));
+    this.onDidUpdate();
   }
 }
