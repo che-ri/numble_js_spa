007642380d33cccfb70669983a10d14b24bb3dca
che-ri <rnjs1135@gmail.com>
2023-01-20 03:07:46 +0900

    [feat] 디테일페이지 라이프사이클 적용 (#9)

--- src/js/pages/DetailPage.js
+++ src/js/pages/DetailPage.js
@@ -15,12 +15,31 @@ export default class DetailPage {
     new Header($("#header"));
   };
 
+  onReady() {
+    //dom이 업데이트된 이후
+    console.log("ready");
+
+    $("#go-home-btn").addEventListener("click", () => {
+      Router.instance.push(`/`);
+    });
+  }
+
+  onWillUpdate() {
+    console.log("willUpdate");
+  }
+
+  onDidUpdate() {
+    console.log("didUpdate");
+  }
+
   template() {
     return `<div><div id="header"></div><h1>상세페이지</h1><button id="go-home-btn">홈</button><div>`;
   }
 
   render() {
+    this.onWillUpdate();
     this.dom.innerHTML = this.template();
     new Header($("#header"));
+    this.onDidUpdate();
   }
 }
